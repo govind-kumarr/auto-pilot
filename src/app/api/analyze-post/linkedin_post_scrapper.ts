179849453f8d71e5c9d6d8f2db7db2282b57c95d
govind <gk4051668@gmail.com>
2025-09-06 21:49:22 +0530

    fix

--- src/app/api/analyze-post/linkedin_post_scrapper.ts
+++ src/app/api/analyze-post/linkedin_post_scrapper.ts
@@ -1,14 +1,28 @@
-import { chromium } from "playwright-core";
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 export async function scrapeLinkedInPost(postUrl: string) {
-  const browser = await chromium.launch({
+  const isLambda = process.env.NODE_ENV === "production";
+
+  const executablePath = isLambda
+    ? await chromium.executablePath()
+    : "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath,
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   try {
     const page = await browser.newPage();
-    await page.goto(postUrl, { waitUntil: "networkidle" });
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.7339.16 Safari/537.36"
+    );
+    await page.setExtraHTTPHeaders({
+      "Accept-Language": "en-US,en;q=0.9",
+    });
+    await page.goto(postUrl, { waitUntil: "networkidle0" });
 
     const postContent = await page.$eval(
       '[data-test-id="main-feed-activity-card__commentary"]',
